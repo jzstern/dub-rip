1261150fed625a5ef68c2f91724d25f77171f3ce
jzstern <jzstern@gmail.com>
2026-01-08 16:59:39 -0700

    fix sentry server setup

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,20 @@
+import * as Sentry from "@sentry/sveltekit";
 import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
 import { sequence } from "@sveltejs/kit/hooks";
+import { env } from "$env/dynamic/private";
+
+Sentry.init({
+	dsn: env.SENTRY_DSN,
+
+	tracesSampleRate: 1.0,
+
+	// Enable logs to be sent to Sentry
+	enableLogs: true,
+
+	// Enable sending user PII (Personally Identifiable Information)
+	// https://docs.sentry.io/platforms/javascript/guides/sveltekit/configuration/options/#sendDefaultPii
+	sendDefaultPii: true,
+});
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
 export const handle = sequence(sentryHandle());
